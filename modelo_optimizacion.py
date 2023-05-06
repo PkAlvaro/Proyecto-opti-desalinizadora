f9357d415604bb3836fa6e4e4026e998e041e8b8
NICOLAS MEDEL <ngmedel@uc.cl>
2023-05-06 18:59:09 -0400

    Funao

--- modelo_optimizacion.py
+++ modelo_optimizacion.py
@@ -6,17 +6,17 @@ from random import randint, uniform
 print('p')
 
 #Definicion de conjuntos#
-Plantas = range(5)
-Tiempo = range(5)
-Region = range(5)
-Metales = range(5)
+Plantas = range(23)
+Tiempo = range(20)
+Region = range(16)
+Metales = range(8)
 
 #Parametros
 
 #Parametros rel. con agua desalinizada y plantas
-#a = {(i,r): randint(0,1) for i in Plantas for r in Region}
+a = {(i,r): randint(0,1) for i in Plantas for r in Region}
 
-a = {(0, 0): 1, (0, 1): 0, (0, 2): 0, (0, 3): 0, (0, 4): 0, (1, 0): 0, (1, 1): 1, (1, 2): 0, (1, 3): 0, (1, 4): 0, (2, 0): 0, (2, 1): 0, (2, 2): 1, (2, 3): 0, (2, 4): 0, (3, 0): 0, (3, 1): 0, (3, 2): 0, (3, 3): 1, (3, 4): 0, (4, 0): 0, (4, 1): 0, (4, 2): 0, (4, 3): 0, (4, 4): 1}
+#a = {(0, 0): 1, (0, 1): 0, (0, 2): 0, (0, 3): 0, (0, 4): 0, (1, 0): 0, (1, 1): 1, (1, 2): 0, (1, 3): 0, (1, 4): 0, (2, 0): 0, (2, 1): 0, (2, 2): 1, (2, 3): 0, (2, 4): 0, (3, 0): 0, (3, 1): 0, (3, 2): 0, (3, 3): 1, (3, 4): 0, (4, 0): 0, (4, 1): 0, (4, 2): 0, (4, 3): 0, (4, 4): 1}
 
 #a = {(0, 0): 1, (0, 1): 0, (1, 0): 0, (1, 1): 1}
 cnt = {(i,t): randint(80, 200) for i in Plantas for t in Tiempo}
@@ -27,8 +27,8 @@ m = {(i,t): randint(40, 100) for i in Plantas for t in Tiempo}
 cta = {(i,t): randint(20, 50) for i in Plantas for t in Tiempo}
 ctr = {(i,t): randint(20, 40) for i in Plantas for t in Tiempo}
 cf = {(i,t): randint(5, 10) for i in Plantas for t in Tiempo}
-aa = {(i,t): randint(1500000, 3000000) for i in Plantas for t in Tiempo}
-cam = {(i,t): randint(1250000, 2000000) for i in Plantas for t in Tiempo}
+aa = {i: randint(1500000, 3000000) for i in Plantas}
+cam = 10**7
 
 #Parametros relacionados con energia
 ce = {(i,t): randint(5, 10) for i in Plantas for t in Tiempo}
@@ -51,7 +51,7 @@ md = {(i,q,t): uniform(0.00002, 0.00006) for i in Plantas for q in Metales for t
 
 cii = {t: uniform(0.05, 0.1) for t in Tiempo}
 capi = {r: uniform(30000000, 500000000) for r in Region}
-M = 10**10
+M = 10**7
 
 #Parametros zpl
 
@@ -86,8 +86,8 @@ model.addConstrs((md[i,q,t] * (x[i,t] + h[i,t]) <= (zpl[i] * dd[q]) + df[q] * (1
 
 model.addConstrs(((x[i,t] + h[i,t]) * w[i,t] <= gem[i,t] for i in Plantas for t in Tiempo), name = "R4" )
 
-model.addConstrs((y[i,t] <= cam[i,t] for i in Plantas for t in Tiempo), name = "R5.1")
-model.addConstrs((y[i,t] <= aa[i,t] for i in Plantas for t in Tiempo), name = "R5.2")
+model.addConstrs((y[i,t] <= cam for i in Plantas for t in Tiempo), name = "R5.1")
+model.addConstrs((y[i,t] <= aa[i] for i in Plantas for t in Tiempo), name = "R5.2")
 
 model.addConstrs((y[i,t] >= 0.6 * x[i,t] for i in Plantas for t in Tiempo), name = "R6.1")
 model.addConstrs((y[i,t] >= 0.8 * h[i,t] for i in Plantas for t in Tiempo), name = "R6.2")
@@ -104,7 +104,7 @@ model.addConstrs((i[r,t] <= capi[r] for r in Region for t in Tiempo), name = "R9
 
 #FUNCION OBJETIVO
 
-objetivo = quicksum ( l[i] * z[i] + quicksum(h[i,t] * cnt[i,t] + ctr[i,t] * x[i,t] + m[i,t] * (x[i,t] + h[i,t]) + ca[i,t] * x[i,t] + cta[i,t] * y[i,t] + ce[i,t] * w[i,t] * (x[i,t] + h[i,t]) + cf[i,t] for t in Tiempo) for i in Plantas) + quicksum(i[r,t] * cii[t] for t in Tiempo for r in Region) + quicksum(zpl[i]*cd[i,q] + (1 - zpl[i])*cff[i,q] for i in Plantas for q in Metales)
+objetivo = quicksum ( l[i] * z[i] + quicksum(h[i,t] * cnt[i,t] + m[i,t] * (x[i,t] + h[i,t]) + ca[i,t] * x[i,t] + cta[i,t] * y[i,t] + ce[i,t] * w[i,t] * (x[i,t] + h[i,t]) + cf[i,t] for t in Tiempo) for i in Plantas) + quicksum(i[r,t] * cii[t] for t in Tiempo for r in Region) + quicksum(zpl[i]*cd[i,q] + (1 - zpl[i])*cff[i,q] for i in Plantas for q in Metales)
 model.setObjective(objetivo, GRB.MINIMIZE)
 
 
@@ -114,7 +114,7 @@ model.setObjective(objetivo, GRB.MINIMIZE)
 model.optimize()
 
 #ATRIBUTOS
-model.printAttr("X")
+#model.printAttr("X")
 
 #model.printAttr("X")
 
